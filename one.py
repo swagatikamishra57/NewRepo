543c2965353f04a07dffd04c668837e85b059ef6
swagatikamishra57 <swagatikamishra57@gmail.com>
2021-03-15 12:58:55 +0530

    added the code of addition

--- one.py
+++ one.py
@@ -1,1 +1,10 @@
-print("Hello")
+print("*"*80)
+print("*"*80)
+print()
+print("Hello world to python".center(80,' '))
+x=5
+y=7
+print(f"x+y={x+y}".center(80,' '))
+print()
+print("*"*80)
+print("*"*80)
